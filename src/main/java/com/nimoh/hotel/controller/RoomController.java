6ebe536d11a34384a79799fa5744253bc3e28562
nimoh <spakers38@gmail.com>
2022-12-27 16:35:46 +0900

    :white_check_mark: Tested to the completed Board Controller method

--- src/main/java/com/nimoh/hotel/controller/RoomController.java
+++ src/main/java/com/nimoh/hotel/controller/RoomController.java
@@ -1,12 +1,10 @@
 package com.nimoh.hotel.controller;
 
-import com.nimoh.hotel.domain.Room;
 import com.nimoh.hotel.service.room.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.List;
 
@@ -21,13 +19,13 @@ public class RoomController {
         this.roomService = roomService;
     }
 
-    @GetMapping("/{roomIdx}")
-    public Room get(@PathVariable int roomIdx) {
-        return roomService.get(roomIdx);
-    }
-
-    @GetMapping("")
-    public List<Room> getList() {
-        return roomService.getList();
-    }
+//    @GetMapping("/{roomIdx}")
+//    public Room get(@PathVariable int roomIdx) {
+//        return roomService.get(roomIdx);
+//    }
+//
+////    @GetMapping("")
+////    public List<Room> getList() {
+////        return roomService.getList();
+////    }
 }
