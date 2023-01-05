19608b5db231ba36124de8b9fdb5fe633814b76b
nimoh <spakers38@gmail.com>
2023-01-05 19:11:45 +0900

    Feat: Add SessionManager

---
+++ src/main/java/com/nimoh/hotel/controller/LoginController.java
@@ -0,0 +1,15 @@
+package com.nimoh.hotel.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class LoginController {
+
+}
