4e58b1b74539505aac374c322275a096ebd1d3a5
nimoh <spakers38@gmail.com>
2023-01-16 20:09:59 +0900

    Feat: Add inquiry reservation controller

--- src/main/java/com/nimoh/hotel/controller/ReservationController.java
+++ src/main/java/com/nimoh/hotel/controller/ReservationController.java
@@ -1,7 +1,40 @@
 package com.nimoh.hotel.controller;
 
+import com.nimoh.hotel.commons.reservation.ReservationException;
+import com.nimoh.hotel.data.dto.reservation.ReservationResponse;
+import com.nimoh.hotel.service.reservation.ReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+import static com.nimoh.hotel.constants.Headers.USER_ID_HEADER;
+
 @RestController
+@RequestMapping("/api/v1/reservation")
 public class ReservationController {
+
+    private final ReservationService reservationService;
+
+    @Autowired
+    public ReservationController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<ReservationResponse>> getReservations(
+            @RequestHeader(USER_ID_HEADER) final Long userId
+            )
+    {
+        try{
+            List<ReservationResponse> result = reservationService.findByUserId(userId);
+            return ResponseEntity.ok().body(result);
+        }catch (ReservationException e){
+            return ResponseEntity.noContent().build();
+        }
+    }
 }
--- src/test/java/com/nimoh/hotel/controller/ReservationControllerTest.java
+++ src/test/java/com/nimoh/hotel/controller/ReservationControllerTest.java
@@ -2,18 +2,26 @@ package com.nimoh.hotel.controller;
 
 import com.google.gson.Gson;
 import com.nimoh.hotel.commons.GlobalExceptionHandler;
+import com.nimoh.hotel.commons.reservation.ReservationErrorResult;
+import com.nimoh.hotel.commons.reservation.ReservationException;
+import com.nimoh.hotel.data.entity.Reservation;
 import com.nimoh.hotel.service.reservation.ReservationServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.Arrays;
+
+import static com.nimoh.hotel.constants.Headers.USER_ID_HEADER;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
@@ -44,6 +52,33 @@ public class ReservationControllerTest {
         );
         //then
         resultActions.andExpect(status().isBadRequest());
+    }
+    @Test
+    public void 예약조회실패_예약내역없음() throws Exception {
+        //given
+        final String url = "/api/v1/reservation";
+        doThrow(new ReservationException(ReservationErrorResult.RESERVATION_NOT_FOUND)).when(reservationService).findByUserId(any());
+        //when
+        ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.get(url)
+                        .header(USER_ID_HEADER,"1234")
+        );
+        //then
+        resultActions.andExpect(status().isNoContent());
+    }
+
+    @Test
+    public void 예약조회성공() throws Exception {
+        //given
+        final String url = "/api/v1/reservation";
+        doReturn(Arrays.asList(Reservation.builder().build())).when(reservationService).findByUserId(any());
+        //when
+        ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.get(url)
+                        .header(USER_ID_HEADER,"1234")
+        );
+        //then
+        resultActions.andExpect(status().isOk());
     }
 
 }
