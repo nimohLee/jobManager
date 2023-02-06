d0a27cad6c5fa7212e8bff3ebfe19aed49c716a2
nimoh <spakers38@gmail.com>
2023-02-07 01:54:10 +0900

    Fix: Modify Request Params in CrawlerServiceImpl recruitCount

--- src/main/java/com/nimoh/jobManager/service/crawler/CrawlerServiceImpl.java
+++ src/main/java/com/nimoh/jobManager/service/crawler/CrawlerServiceImpl.java
@@ -3,7 +3,6 @@ package com.nimoh.jobManager.service.crawler;
 import com.nimoh.jobManager.commons.crawler.CrawlerErrorResult;
 import com.nimoh.jobManager.commons.crawler.CrawlerException;
 import com.nimoh.jobManager.data.dto.crawler.JobCrawlerDto;
-import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -15,27 +14,24 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Service
-public class CralwerServiceImpl implements CrawlerService{
+public class CrawlerServiceImpl implements CrawlerService{
 
-    Logger logger = LoggerFactory.getLogger(CralwerServiceImpl.class);
+    Logger logger = LoggerFactory.getLogger(CrawlerServiceImpl.class);
 
     public List<JobCrawlerDto> getSearchList(Map<String, String> searchOption) throws IOException {
-        if(searchOption.get("searchWord")==null||searchOption.get("recruitPage")==null||searchOption.get("recruitPageCount")==null||searchOption.get("recruitSort")==null){
+        if(searchOption.get("searchWord")==null||searchOption.get("recruitPage")==null||searchOption.get("recruitSort")==null){
             throw new CrawlerException(CrawlerErrorResult.OPTION_NULL_EXCEPTION);
         }
-        final String searchList = "https://www.saramin.co.kr/zf_user/search/recruit?search_done=y&search_optional_item=n&company_cd=0%2C1%2C2%2C3%2C4%2C5%2C6%2C7%2C9%2C10&show_applied=&quick_apply=&except_read=&ai_head_hunting=&mainSearch=n&loc_mcd=101000&inner_com_type="
+        final String searchList = "https://www.saramin.co.kr/zf_user/search/recruit?search_done=y&search_optional_item=n&company_cd=0%2C1%2C2%2C3%2C4%2C5%2C6%2C7%2C9%2C10&show_applied=&quick_apply=&except_read=&ai_head_hunting=&mainSearch=n&loc_mcd=101000&inner_com_type=&recruitPageCount=20"
                 + "&searchword=" +searchOption.get("searchWord")
                 + "&recruitPage=" + searchOption.get("recruitPage")
-                + "&recruitPageCount="+ searchOption.get("recruitPageCount")
                 + "&recruitSort=" + searchOption.get("recruitSort");
         logger.info(searchList);
         Connection conn = Jsoup.connect(searchList);
-        List<JobCrawlerDto> result = new ArrayList<>();
         try {
             Document document = conn.get();
             List<JobCrawlerDto> jobList = getJobInfo(document); // 칼럼명
@@ -49,6 +45,8 @@ public class CralwerServiceImpl implements CrawlerService{
     }
     private List<JobCrawlerDto> getJobInfo(Document document) {
         Elements itemRecruit = document.select(".item_recruit");
+        logger.info(String.valueOf(itemRecruit));
+        logger.info("itemRecruit size = "+String.valueOf(itemRecruit.size()));
         List<JobCrawlerDto> jobInfo = new ArrayList<>();
         for (Element element : itemRecruit) {
             String title = element.select(".area_job .job_tit a").attr("title");
