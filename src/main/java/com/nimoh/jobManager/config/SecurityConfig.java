90190c2caf9664cd2f2304ce01b5c9d10016e466
nimoh <spakers38@gmail.com>
2023-02-15 23:40:26 +0900

    Feat: Add SecurityConfig

--- src/main/java/com/nimoh/jobManager/config/SecurityConfig.java
+++ src/main/java/com/nimoh/jobManager/config/SecurityConfig.java
@@ -2,15 +2,46 @@ package com.nimoh.jobManager.config;
 
 import com.nimoh.jobManager.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
     private final UserRepository userRepository;
-    private final CorsConfiguration configuration;
+    private final CorsConfig corsConfig;
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf().disable()
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .apply(new MyCustomDsl())
+                .and()
+                .authorizeRequests(authorize ->  authorize.antMatchers(("/api/v1/job/**"))
+                        .authenticated()
+                        .anyRequest().permitAll()
+                ).build();
+    }
+
+    public class MyCustomDsl extends AbstractHttpConfigurer<MyCustomDsl, HttpSecurity> {
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            AuthenticationManager authenticationManager = http.getSharedObject(AuthenticationManager.class);
+            http
+                    .addFilter(corsConfig.corsFilter());
+//                    .addFilter(new JwtAuthenticationFilter(authenticationManager))
+//                    .addFilter(new JwtAuthorizationFilter(authenticationManager, userRepository));
+        }
+    }
 
 }
