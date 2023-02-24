6f4adaf0021cf2ceb04445e564abe7133278d09a
nimoh <spakers38@gmail.com>
2023-02-24 11:36:00 +0900

    Feat: Add Loading progress in Login.tsx

--- client/src/component/page/Login.tsx
+++ client/src/component/page/Login.tsx
@@ -3,13 +3,15 @@ import axios from 'axios';
 import { FormEvent, useState } from 'react';
 import { useEffect } from 'react';
 import { Cookies } from 'react-cookie';
+import { Spinner } from 'react-bootstrap';
+import Loading from './Loading';
 
 function Login() {
     const [id, setId] = useState<string>();
     const [password, setPassword] = useState<string>();
     const cookie = new Cookies();
     const accessCookie = cookie.get("accessCookie");
-
+    const [isLoading, setIsLoading] = useState(false);
     useEffect(()=>{
         if(accessCookie){
             alert("이미 로그인된 상태입니다");
@@ -27,6 +29,7 @@ function Login() {
 
     const postLogin = async (e:FormEvent<HTMLFormElement>) =>{
         e.preventDefault();
+        setIsLoading(true);
         const url = "/api/v1/user/login";
         const data = {
             uid : id?.trim(),
@@ -45,29 +48,36 @@ function Login() {
             window.location.href="/";
         }catch(err){
             alert("아이디 또는 비밀번호가 잘못되었습니다");
+        }finally{
+            setIsLoading(false);
         }
     }
 
     return (
         <main className='flex justify-center'>
-            <section className='py-16 px-32 top-0 relative border border-b flex flex-column'>
-            <form onSubmit={postLogin}>
-                <div className="mb-3 flex flex-column">
-                    <label className='mb-2'>아이디</label>
-                    <input type="ID" placeholder="Enter ID"  className='px-3 py-2 border border-solid' onChange={onChange}/>
-                </div>
-
-                <div className="mb-3 flex flex-column">
-                    <label className='mb-2'>비밀번호</label>
-                    <input type="Password" placeholder="Password"  className='px-3 py-2 border border-solid' onChange={onChange}/>
-                </div>
-
-                <Button variant="secondary" type="submit" className='float-right'>
-                    로그인
-                </Button>
-            </form>
-            <a href="/signUp" className='text-neutral-400 mt-3'>아직 회원이 아니신가요?</a>
-            </section>
+            {
+                isLoading?
+                <Loading/>
+                :<section className='py-16 px-32 top-0 relative border border-b flex flex-column'>
+                <form onSubmit={postLogin}>
+                    <div className="mb-3 flex flex-column">
+                        <label className='mb-2'>아이디</label>
+                        <input type="ID" placeholder="Enter ID"  className='px-3 py-2 border border-solid' onChange={onChange}/>
+                    </div>
+    
+                    <div className="mb-3 flex flex-column">
+                        <label className='mb-2'>비밀번호</label>
+                        <input type="Password" placeholder="Password"  className='px-3 py-2 border border-solid' onChange={onChange}/>
+                    </div>
+    
+                    <Button variant="secondary" type="submit" className='float-right'>
+                        로그인
+                    </Button>
+                </form>
+                <a href="/signUp" className='text-neutral-400 mt-3'>아직 회원이 아니신가요?</a>
+                </section>
+            }
+            
         </main>
     );
 }
