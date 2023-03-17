84cfabfea900aa7154fcb83a94742a6beee180b2
nimoh <spakers38@gmail.com>
2023-03-17 22:40:32 +0900

    Docs: Add javadoc in CrawlerService

--- src/main/java/com/nimoh/jobManager/service/crawler/impl/JobSearchServiceImpl.java
+++ src/main/java/com/nimoh/jobManager/service/crawler/impl/JobSearchServiceImpl.java
@@ -15,12 +15,25 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * 서비스 - 구직사이트 직무 검색
+ *
+ * @author nimoh
+ */
 @Service
 @RequiredArgsConstructor
 public class JobSearchServiceImpl implements JobSearchService {
     private final JsoupConnection jsoupConnection;
     private final Map<String, JobCrawler> crawlerMap;
 
+    /**
+     * 검색옵션을 받아 크롤링 후 결과 반환
+     *
+     * @param searchOption 검색 옵션이 들어있는 Map
+     * @param crawlerName 구직사이트 별 크롤러 이름(Bean 이름) ex) saraminCrawler, jobKoreaCralwer...
+     * @throws CrawlerException 검색 옵션이 null이거나, 정렬 옵션이 유요하지 않은 경우, jsoup 관련 IO예외 시 예외 발생
+     * @return 검색결과가 List에 담겨 반환
+     */
     @Override
     public List<JobCrawlerDto> getSearchList(Map<String, String> searchOption, String crawlerName) {
         JobCrawler jobCrawler = crawlerMap.get(crawlerName);
